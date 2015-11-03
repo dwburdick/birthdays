a5a3b4e71d7e457bb03e00af9410792ac6ee30a7
Dave Burdick <daveburdick@gmail.com>
2015-11-03 10:42:35 -0700

    Display current age, change on slide

--- www/js/birthdays.js
+++ www/js/birthdays.js
@@ -1,26 +1,41 @@
-var birthday = function(){
-	var model = {
+var model = {
 
-	};
+};
 
-	var controller = {
+var controller = {
+	currentAge: 51,
+};
 
-	};
+var viewSlider = {
+	render: function() {
+		$("#slider").slider({
+			max: 103,
+			min: 1,
+			step: 1,
+			value: controller.currentAge,
+			change: function(event, ui) {
+				currentAge = ui.value;
+				viewAge.render();
+			},
+			slide: function(event, ui) {
+				controller.currentAge = ui.value;
+				viewAge.render();
+			},
+		});
+		viewAge.render();
+	},
+};
 
-	var viewSlider = {
-		render: function() {
-			$("#slider").slider({
-				max: 103,
-				min: 1,
-			});
-		},
-	};
-
-	var viewResults = {
-		render: function() {
-
-		},
+var viewAge = {
+	render: function() {
+		$("#age").text(controller.currentAge);
 	}
-
-	viewSlider.render();
 }
+
+var viewResults = {
+	render: function() {
+
+	},
+}
+
+viewSlider.render();
