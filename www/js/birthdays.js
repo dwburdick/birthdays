0556d035672038da353d856d091504992596f644
Dave Burdick <daveburdick@gmail.com>
2015-11-03 10:11:50 -0700

    Initial commit.

---
+++ www/js/birthdays.js
@@ -0,0 +1,26 @@
+var birthday = function(){
+	var model = {
+
+	};
+
+	var controller = {
+
+	};
+
+	var viewSlider = {
+		render: function() {
+			$("#slider").slider({
+				max: 103,
+				min: 1,
+			});
+		},
+	};
+
+	var viewResults = {
+		render: function() {
+
+		},
+	}
+
+	viewSlider.render();
+}
