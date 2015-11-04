68149373e4165f0a44770a83193e3752f67a81fd
Dave Burdick <daveburdick@gmail.com>
2015-11-04 16:43:24 -0700

    Next result button exists for multi-response ages, but isn't yet working

--- www/js/birthdays.js
+++ www/js/birthdays.js
@@ -1,69 +1,87 @@
 var birthdayInit = function(defaultAge) {
-	var model = {
-	ageResult: {
-		1: "One",
-		2: "Two",
-		3: "Three",
-		4: "Four",
-		5: "Five",
-		6: "Six",
-		49: [
-			"Forty-nine",
-			"Forty and nine",
-			"One fewer than fifty"
-		],
-		50: "Fifty",
-		51: "Fifty-one"
-	},
-};
+	model = {
+		ageResult: {
+			1: "One",
+			2: "Two",
+			3: "Three",
+			4: "Four",
+			5: "Five",
+			6: "Six",
+			49: [
+				"Forty-nine",
+				"Forty and nine",
+				"One fewer than fifty"
+			],
+			50: "Fifty",
+			51: "Fifty-one"
+		},
+	};
 
-var controller = {
-	currentAge: defaultAge,
-	getAgeResults: function(age) {
-		displayResult = model.ageResult[age];
-		if (model.ageResult[age] instanceof Array == false) {
-			printResult = displayResult;
-		} else { 
-			printResult = "prev. " + displayResult[0] + " next";
-		};
-	},	
-};
+	controller = {
+		init: function() {
+			currentResultIndex = 0;
+		},
+		currentAge: defaultAge,
+		getAgeResults: function(age) {
+			displayResult = model.ageResult[age];
+			if (model.ageResult[age] instanceof Array == false) {
+				printResult = displayResult;
+			} else { 
+				printResult = "prev. " + displayResult[currentResultIndex] + " <input type='button' value='Next' id='nextResult'>";
+				controller.nextResult();
+			};
+		},
+		nextResult: function() {
+			$(document).ready(function(){
+				alert("triggered");
+				$("#nextResult").click(function(){
+					currentResultIndex++;
+					viewResult.render();
+				});
+			});
+		},
+		prevResult: function() {
 
-var viewSlider = {
-	render: function() {
-		$("#slider").slider({
-			max: 103,
-			min: 1,
-			step: 1,
-			value: controller.currentAge,
-			change: function(event, ui) {
-				currentAge = ui.value;
-				viewAge.render();
-				viewResult.render();				
-			},
-			slide: function(event, ui) {
-				controller.currentAge = ui.value;
-				viewAge.render();
-				viewResult.render();
-			},
-		});
-		viewAge.render();
-		viewResult.render();
-	},
-};
+		},
+	};
 
-var viewAge = {
-	render: function() {
-		$("#age").text(controller.currentAge);
-	}
-}
+	viewSlider = {
+		render: function() {
+			$("#slider").slider({
+				max: 103,
+				min: 1,
+				step: 1,
+				value: controller.currentAge,
+				change: function(event, ui) {
+					currentAge = ui.value;
+					viewAge.render();
+					viewResult.render();				
+				},
+				slide: function(event, ui) {
+					controller.currentAge = ui.value;
+					viewAge.render();
+					viewResult.render();
+				},
+			});
+			viewAge.render();
+			viewResult.render();
+		},
+	};
 
-var viewResult = {
-	render: function() {
-		controller.getAgeResults(controller.currentAge);
-		$("#result").text(printResult);
-	},
-}
+	viewAge = {
+		render: function() {
+			$("#age").text(controller.currentAge);
+		}
+	};
 
-viewSlider.render();
+	viewResult = {
+		render: function() {
+			controller.getAgeResults(controller.currentAge);
+			$("#result").empty();
+			$("#result").append(printResult);
+		},
+	};
+
+	controller.init();
+	viewSlider.render();
 }
